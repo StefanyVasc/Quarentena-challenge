1fd8a9f53e7fef49b44fac5cc7d01094b3d4dc32
StefanyVasc <stefany.vasc.sa@gmail.com>
2020-04-13 21:49:01 -0300

    :construction: including smooth scrolling to sections
    smooth scrolling for links of menu associate the sections

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,24 @@ function initAccordion() {
 }
 
 initAccordion();
+
+function initScrollSuave() {
+  const linkInternos = document.querySelectorAll('a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+  linkInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+
+initScrollSuave();
