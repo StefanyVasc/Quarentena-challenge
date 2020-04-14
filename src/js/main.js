7ca589716a73002248b8b9e7866bece31d900a6e
StefanyVasc <stefany.vasc.sa@gmail.com>
2020-04-13 21:31:39 -0300

    :construction: included faq accordion animation
    included faq animation with .css and .js

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const classActive = "active";
+  if (accordionList.length) {
+    accordionList[0].classList.add(classActive);
+    accordionList[0].nextElementSibling.classList.add(classActive);
+
+    function activeAccordion() {
+      this.classList.toggle(classActive);
+      this.nextElementSibling.classList.toggle(classActive);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+
+initAccordion();
