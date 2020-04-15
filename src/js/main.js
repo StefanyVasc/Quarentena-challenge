d516f9f0dfab3dbf8688c01aa50bb391b3ef97f4
StefanyVasc <stefany.vasc.sa@gmail.com>
2020-04-14 21:22:09 -0300

    :construction: including scroll animation
    when scrolling down the sections are revealed with animation

--- src/js/main.js
+++ src/js/main.js
@@ -38,3 +38,29 @@ function initScrollSuave() {
 }
 
 initScrollSuave();
+
+function initAnimateScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    const windowHalf = window.innerHeight * 0.75;
+
+    function animateScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowHalf;
+
+        if (isSectionVisible < 0) {
+          section.classList.add("active");
+        } else {
+          section.classList.remove("active");
+        }
+      });
+    }
+    animateScroll();
+
+    window.addEventListener("scroll", animateScroll);
+  }
+}
+
+initAnimateScroll();
