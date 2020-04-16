8a9b5f07cdb450f120246e6076bbfa3628420276
StefanyVasc <stefany.vasc.sa@gmail.com>
2020-04-16 00:44:02 -0300

    :construction: file for populate gallery
    this file is only json structure with objects

---
+++ src/js/assets.js
@@ -0,0 +1,58 @@
+let imagensJSON = {
+  imagens: [
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/c_scale,w_224/v1586924505/galeria/swiper/img-28_i2lepx.png",
+    },
+    {
+      titulo: "teste wide",
+      descricao: "imagen de teste widescreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/c_scale,w_224/v1586924505/galeria/swiper/img-30_to30rx.png",
+    },
+    {
+      titulo: "teste wide",
+      descricao: "imagen de teste widescreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/c_scale,w_224/v1586924503/galeria/swiper/img-29_thwp8v.png",
+    },
+    {
+      titulo: "teste wide",
+      descricao: "imagen de teste widescreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/c_scale,w_224/v1586924502/galeria/swiper/img-27_vrxfct.png",
+    },
+    {
+      titulo: "teste wide",
+      descricao: "imagen de teste widescreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/c_scale,w_224/v1586924501/galeria/swiper/img-26_pstlbd.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/c_scale,w_224/v1586924501/galeria/swiper/img-24_tkxpss.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/c_scale,w_224/v1586924501/galeria/swiper/img-25_uhzbel.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/c_scale,w_224/v1586924501/galeria/swiper/img-23_d0sqlp.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/c_scale,w_224/v1586924501/galeria/swiper/img-22_s8kejb.png",
+    },
+  ],
+};
