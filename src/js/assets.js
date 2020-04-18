f68f0e28c5d9e410daa471f35e3a7f76fc33a880
StefanyVasc <stefany.vasc.sa@gmail.com>
2020-04-17 21:19:02 -0300

    :construction: progress in galeery

--- src/js/assets.js
+++ src/js/assets.js
@@ -4,55 +4,182 @@ let imagensJSON = {
       titulo: "teste full",
       descricao: "imagen de teste fullscreen",
       url:
-        "https://res.cloudinary.com/stevasc/image/upload/c_scale,w_224/v1586924505/galeria/swiper/img-28_i2lepx.png",
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924505/galeria/swiper/img-28_i2lepx.png",
     },
     {
       titulo: "teste wide",
       descricao: "imagen de teste widescreen",
       url:
-        "https://res.cloudinary.com/stevasc/image/upload/c_scale,w_224/v1586924505/galeria/swiper/img-30_to30rx.png",
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924505/galeria/swiper/img-30_to30rx.png",
     },
     {
       titulo: "teste wide",
       descricao: "imagen de teste widescreen",
       url:
-        "https://res.cloudinary.com/stevasc/image/upload/c_scale,w_224/v1586924503/galeria/swiper/img-29_thwp8v.png",
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924503/galeria/swiper/img-29_thwp8v.png",
     },
     {
       titulo: "teste wide",
       descricao: "imagen de teste widescreen",
       url:
-        "https://res.cloudinary.com/stevasc/image/upload/c_scale,w_224/v1586924502/galeria/swiper/img-27_vrxfct.png",
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924502/galeria/swiper/img-27_vrxfct.png",
     },
     {
       titulo: "teste wide",
       descricao: "imagen de teste widescreen",
       url:
-        "https://res.cloudinary.com/stevasc/image/upload/c_scale,w_224/v1586924501/galeria/swiper/img-26_pstlbd.png",
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924501/galeria/swiper/img-26_pstlbd.png",
     },
     {
       titulo: "teste full",
       descricao: "imagen de teste fullscreen",
       url:
-        "https://res.cloudinary.com/stevasc/image/upload/c_scale,w_224/v1586924501/galeria/swiper/img-24_tkxpss.png",
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924501/galeria/swiper/img-24_tkxpss.png",
     },
     {
       titulo: "teste full",
       descricao: "imagen de teste fullscreen",
       url:
-        "https://res.cloudinary.com/stevasc/image/upload/c_scale,w_224/v1586924501/galeria/swiper/img-25_uhzbel.png",
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924501/galeria/swiper/img-25_uhzbel.png",
     },
     {
       titulo: "teste full",
       descricao: "imagen de teste fullscreen",
       url:
-        "https://res.cloudinary.com/stevasc/image/upload/c_scale,w_224/v1586924501/galeria/swiper/img-23_d0sqlp.png",
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924501/galeria/swiper/img-23_d0sqlp.png",
     },
     {
       titulo: "teste full",
       descricao: "imagen de teste fullscreen",
       url:
-        "https://res.cloudinary.com/stevasc/image/upload/c_scale,w_224/v1586924501/galeria/swiper/img-22_s8kejb.png",
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924501/galeria/swiper/img-22_s8kejb.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924500/galeria/swiper/img-21_sebpsz.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924500/galeria/swiper/img-20_wwmj5n.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924500/galeria/swiper/img-19_ninylj.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924500/galeria/swiper/img-18_tpokrb.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924499/galeria/swiper/img-16_imzfz4.png",
+    },
+
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924499/galeria/swiper/img-17_t4ny8r.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924499/galeria/swiper/img-15_teobzi.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924499/galeria/swiper/img-14_mqgqxw.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924498/galeria/swiper/img-13_wtk4tj.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924498/galeria/swiper/img-9_xb7ysu.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924498/galeria/swiper/img-12_zp68ni.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924498/galeria/swiper/img-11_uvehvv.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924498/galeria/swiper/img-10_ejru31.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924497/galeria/swiper/img-8_wi5fhs.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924497/galeria/swiper/img-6_clndcc.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924497/galeria/swiper/img-7_kmusvr.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924497/galeria/swiper/img-5_wakpqi.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924496/galeria/swiper/img-4_bd2rky.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924496/galeria/swiper/img-3_visyni.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924496/galeria/swiper/img-1_ftu3fq.png",
+    },
+    {
+      titulo: "teste full",
+      descricao: "imagen de teste fullscreen",
+      url:
+        "https://res.cloudinary.com/stevasc/image/upload/v1586924496/galeria/swiper/img-2_xpwkby.png",
     },
   ],
 };
