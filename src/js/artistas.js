c8a7590d915e1ecd477c2fcc75eb949d19e7f926
StefanyVasc <stefany.vasc.sa@gmail.com>
2020-04-26 02:37:18 -0300

    :sparkles: starting construction page artists

---
+++ src/js/artistas.js
@@ -0,0 +1,129 @@
+let artistasJSON = {
+  artistas: [
+    {
+      artista: "tago",
+      dias: {
+        dia1: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia2: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia3: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia4: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia5: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia6: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia7: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia8: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia9: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia10: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia11: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia12: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia13: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia14: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia15: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia16: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia17: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia18: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia19: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia20: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia21: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia22: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia23: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia24: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia25: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia26: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia27: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia28: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia29: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+        dia30: {
+          tema: "exemplo",
+          url: "exemplo",
+        },
+      },
+    },
+  ],
+};
